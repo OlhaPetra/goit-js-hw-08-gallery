2b32876f91f0b428c38901f0e93db0e788123360
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-08-14 16:39:55 +0300

    add esc + overlay

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import galleryItems from './app.js';
 const galleryContainer = document.querySelector('.js-gallery');
 const galleryMarkup = createGallery(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-
 const modalLightbox = document.querySelector('.lightbox');
 const LightboxOverlay = document.querySelector('.lightbox__overlay');
 const lightboxImage = document.querySelector('.lightbox__image');
 const buttonCloseLightbox = document.querySelector('[data-action="close-lightbox"]');
 
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
 galleryContainer.addEventListener('click', onGalleryClick);
 buttonCloseLightbox.addEventListener('click', onCloseModal);
-
+LightboxOverlay.addEventListener('click', onOverlayClick)
 
 function createGallery(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
@@ -52,11 +52,26 @@ function onGalleryClick(e) {
 }
 
 function onOpenModal() {
+    window.addEventListener('keydown', onEscKeyPress);
     modalLightbox.classList.add('is-open');
 }
 
-function onCloseModal(e) {
+function onCloseModal() {
+    window.removeEventListener('keydown', onEscKeyPress);
+
     modalLightbox.classList.remove('is-open');
     lightboxImage.removeAttribute('src');
     lightboxImage.removeAttribute('alt');
+}
+
+function onOverlayClick(e) {
+    if (e.target === e.currentTarget) {
+        onCloseModal()
+    }
+}
+
+function onEscKeyPress(e) {
+    if (e.code === 'Escape') {
+        onCloseModal()
+    }
 }
